a6982e0eb667f6e048a2fba94f1e4c5b3ef3fea0
dev6-7 <albert0110011@gmail.com>
2021-12-08 09:00:57 +0300

    Ktlint checker

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
+
+    var v1: String? = null
+    println(v1?.length ?: "1123")
 }
